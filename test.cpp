02cbae41322618fdc67244fd482f82ceb6309818
Adir <adirofir123@gmail.com>
2025-04-02 10:59:52 +0300

    Added doctests!

--- test.cpp
+++ test.cpp
@@ -1,42 +1,31 @@
-//adirofir123@gmail.com
-#include <iostream>
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
 #include "Graph.hpp"
 #include "Algorithms.hpp"
 
 using namespace graph;
 
-void test_basic_graph() {
+TEST_CASE("Graph basic edge operations") {
     Graph g(4);
     g.addEdge(0, 1);
     g.addEdge(1, 2);
-    g.addEdge(2, 3);
-    g.removeEdge(2, 3);
 
-    try {
-        g.removeEdge(2, 3); // Should throw
-        std::cout << "Failed: removeEdge did not throw on missing edge\n";
-    } catch (...) {
-        std::cout << "Passed: removeEdge throws on missing edge\n";
-    }
+    CHECK(g.hasEdge(0, 1));
+    CHECK(g.hasEdge(1, 2));
+    CHECK(!g.hasEdge(0, 2));
+
+    g.removeEdge(1, 2);
+    CHECK(!g.hasEdge(1, 2));
 }
 
-void test_bfs() {
+TEST_CASE("Graph BFS") {
     Graph g(3);
     g.addEdge(0, 1);
     g.addEdge(1, 2);
 
     Graph bfsTree = Algorithms::bfs(g, 0);
-    if (bfsTree.hasEdge(0, 1) && bfsTree.hasEdge(1, 2)) {
-        std::cout << "Passed: BFS tree includes expected edges\n";
-    } else {
-        std::cout << "Failed: BFS tree missing expected edges\n";
-    }
-}
 
-int main() {
-    std::cout << "[Running Tests]\n";
-    test_basic_graph();
-    test_bfs();
-    std::cout << "[Done]\n";
-    return 0;
+    CHECK(bfsTree.hasEdge(0, 1));
+    CHECK(bfsTree.hasEdge(1, 2));
+    CHECK(!bfsTree.hasEdge(0, 2));
 }
